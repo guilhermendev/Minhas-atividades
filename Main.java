e087b69a7b97c6c05137ddc95b11e310e489dc3d
Guilherme Souza Nogueira <guilherme10.nogueira@gmail.com>
2026-03-04 11:57:08 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-       Scanner num =  new Scanner(System.in);
+        Scanner num = new Scanner(System.in);
+        System.out.println("Digite o numero da tabuada que deseja visualizar: ");
+        int numero = num.nextInt();
+        System.out.println("Tabuada do " + numero + ":");
 
-        System.out.print("Digite a idade do empregado: ");
-        int idade = num.nextInt();
-        System.out.print("Digite o tempo de trabalho (anos): ");
-        int tempoTrabalho = num.nextInt();
-
-        if (idade >= 65 || tempoTrabalho >= 30 || (idade >= 60 && tempoTrabalho >= 25)) {
-            System.out.println("Requerer aposentadoria");
-        } else {
-            System.out.println("Não requerer");
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
         }
     }
 }
