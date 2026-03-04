007c171fc197d8c255bc8a8b37fbe030ca56e1b2
Guilherme Souza Nogueira <guilherme10.nogueira@gmail.com>
2026-03-04 11:57:31 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,13 +2,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
-        System.out.println("Digite o numero da tabuada que deseja visualizar: ");
-        int numero = num.nextInt();
-        System.out.println("Tabuada do " + numero + ":");
 
-        for (int i = 1; i <= 10; i++) {
-            int resultado = numero * i;
-            System.out.println(numero + " x " + i + " = " + resultado);
+        System.out.print("Digite o número inicial: ");
+        int inicio = num.nextInt();
+
+        System.out.print("Digite o número final: ");
+        int fim = num.nextInt();
+
+        int soma = 0;
+        int contador = 0;
+
+        for (int i = inicio; i <= fim; i++) {
+            soma += i;
+            contador++;
         }
+
+        if (contador > 0) {
+            double media = (double) soma / contador;
+            System.out.println("A média entre " + inicio + " e " + fim + " é: " + media);
+        } else {
+            System.out.println("Intervalo inválido (o início deve ser menor que o fim).");
+        }
+        
     }
 }
